14839874e3b9eec7b4a3f783c8a29e38415a8a80
Ilya Mizus <bigfatbrowncat@gmail.com>
2015-09-14 22:08:01 +0300

    Animatable changed

--- src/java/firststep/contracts/Animatable.java
+++ src/java/firststep/contracts/Animatable.java
@@ -1,6 +1,15 @@
 package firststep.contracts;
 
 public interface Animatable extends Renderable {
-	void setCurrentFrame(int index);
-	int getFrameCount();
+	/**
+	 * Sets the current time
+	 * @param time in seconds
+	 */
+	void setCurrentTime(float time);
+	
+	/**
+	 * @return the animatable duration in seconds.
+	 * If the animation is infinite, returns null
+	 */
+	Float getDuration();
 }
